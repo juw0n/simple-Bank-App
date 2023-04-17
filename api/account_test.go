1c54112857e2b6f44804c9684e4b7a69a5d5faea
Oluwajuwon ODUNITAN <oduns07@gmail.com>
2023-04-17 17:31:07 +0100

    update mockdb testing file

--- api/account_test.go
+++ api/account_test.go
@@ -1,11 +1,19 @@
 package api
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
 	"github.com/golang/mock/gomock"
+	mockdb "github.com/juw0n/simpleBank/db/mock"
+	db "github.com/juw0n/simpleBank/db/sqlc"
 	"github.com/juw0n/simpleBank/util"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestGetAccountAPI(t *testing.T){
+func TestGetAccountAPI(t *testing.T) {
 	account := randomAccount()
 
 	ctrl := gomock.NewController(t)
@@ -14,14 +22,26 @@ func TestGetAccountAPI(t *testing.T){
 	store := mockdb.NewMockStore(ctrl)
 	// build stubs
 	store.EXPECT().
-		GetAccount(gomock.Any(), gomock.Eq(account.ID))
+		GetAccount(gomock.Any(), gomock.Eq(account.ID)).Times(1).Return(account, nil)
+
+	// Start test server and send request
+	server := NewServer(store)
+	recorder := httptest.NewRecorder()
+
+	url := fmt.Sprintf("/accounts/%d", account.ID)
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	assert.NoError(t, err)
+
+	server.router.ServeHTTP(recorder, request)
+	// check response
+	assert.Equal(t, http.StatusOK, recorder.Code)
 }
 
 func randomAccount() db.Account {
 	return db.Account{
-		ID: util.RandomInt(1, 1000),
-		Owner: util.RandomOwner(),
-		Balance: util.RandomMoney(),
+		ID:       util.RandomInt(1, 1000),
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
 }
